c686078cfc604d31d21e020dca7df29a34206898
Victor <vac@paloma.es>
2019-02-06 12:39:45 +0100

    subir objetos

---
+++ Objeto.java
@@ -0,0 +1,13 @@
+public class Objeto{
+	public int numero;
+	public String texto;
+
+	public Objeto(int numero,String texto){
+		this.numero=numero;
+		this.texto=texto;
+	}
+
+	public String toString(){
+		return "Numero "+numero+" texto "+texto
+	}
+}
---
+++ Principal.java
@@ -0,0 +1,7 @@
+public class Principal{
+
+	public static void main(String args[]){
+		Objeto o1=new Objeto(12,"hola");
+		o1.toString();
+	}
+}
